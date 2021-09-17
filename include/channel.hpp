10e090e1d7f693b17ff3d398827dc72048230e3b
Александр Брюханов <greed@ispsystem.com>
2021-09-17 17:50:24 +0800

    feat: channels

--- include/channel.hpp
+++ include/channel.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include <experimental/coroutine>
 #include <queue>
+#include <task.hpp>
 #include <variant>
 
 using handle = std::experimental::coroutine_handle<>;
 
-template <typename Type>
+template <typename Container>
+auto pop(Container &container) -> typename Container::value_type {
+    auto value = std::move(container.front());
+    container.pop();
+    return std::move(value);
+}
+
+template <typename Type, typename Tasks = task>
 class channel {
 public:
     using value_type = Type;
@@ -16,15 +24,13 @@ public:
         void close() {}
 
         bool await_ready() const { return m_self->m_capacity > m_self->m_queue.size(); }
-        void await_suspend(handle coro) {
-            m_self->m_in_queue.emplace(coro);
-        }
+        void await_suspend(handle coro) { m_self->m_in_queue.emplace(coro); }
         auto await_resume() {
             struct helper {
                 channel *m_self;
                 void operator << (value_type value) {
                     m_self->m_queue.template emplace(std::move(value));
-                    // TODO: resume writer
+                    channel::resume(m_self->m_out_queue);
                 }
             };
             return helper{m_self};
@@ -39,13 +45,10 @@ public:
     public:
         using value_type = channel::value_type;
         bool await_ready() const { return !m_self->m_queue.empty(); }
-        void await_suspend(handle coro) {
-            m_self->m_out_queue.emplace(coro);
-        }
+        void await_suspend(handle coro) { m_self->m_out_queue.emplace(coro); }
         value_type await_resume() {
-            auto value = std::move(m_self->m_queue.front());
-            m_self->m_queue.pop();
-            // TODO: resume reader
+            auto value = pop(m_self->m_queue);
+            channel::resume(m_self->m_in_queue);
             return std::move(value);
         }
     private:
@@ -63,6 +66,12 @@ private:
     std::queue<value_type> m_queue;
     std::queue<handle> m_in_queue;
     std::queue<handle> m_out_queue;
+
+    static void resume(std::queue<handle> &queue) {
+        if (!queue.empty()) {
+            task::post([coro = pop(queue)] () mutable { coro.resume(); });
+        }
+    }
 };
 
 template <typename ...Channels>
