58b9b491d012b3c65b992a8ba0fd241d82a4c602
Александр Брюханов <greed@ispsystem.com>
2021-09-17 17:14:28 +0800

    feat: tasks

--- include/task.hpp
+++ include/task.hpp
@@ -4,10 +4,7 @@
 class task {
 public:
     struct promise_type {
-        using handle = std::experimental::coroutine_handle<promise_type>;
-        using suspend_always = std::experimental::suspend_always;
         using suspend_never = std::experimental::suspend_never;
-
         task get_return_object() noexcept { return {}; }
         void return_void() noexcept { }
         suspend_never initial_suspend() noexcept { return {}; }
@@ -15,7 +12,22 @@ public:
         void unhandled_exception() { std::terminate(); }
     };
 
-    static void run() {}
+    static void post(std::function<void()> work) {
+        todo().emplace_back(std::move(work));
+    }
+
+    static void run() {
+        while (!todo().empty()) {
+            auto last = std::move(todo().back());
+            todo().pop_back();
+            last();
+        }
+    }
+
 private:
     task() noexcept = default;
+    static std::vector<std::function<void()>> &todo() {
+        static std::vector<std::function<void()>> result;
+        return result;
+    }
 };
